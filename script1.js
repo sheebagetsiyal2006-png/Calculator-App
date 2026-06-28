660b2545c1da7875f8669f082f01d710aae506df
sheeba <sheebagetsiyal2006@gmail.com>
2026-06-28 21:42:41 +0530

    commit 1

---
+++ script1.js
@@ -0,0 +1,19 @@
+function addValue(value) 
+{
+    document.getElementById("display").value+=value;
+}
+
+function calculate(){
+    let expression = document.getElementById("display").value;
+
+    let result = eval(expression);
+
+document.getElementById("display").value=result;
+}
+function clearDisplay(){
+    document.getElementById("display").value="";
+}
+
+
+
+
